1610c5d241ff4491ef0e3f61eea38299229f081a
Anthony Francis <gday@axf.id.au>
2026-07-22 19:38:11 +1000

    add major projects

--- main.go
+++ main.go
@@ -8,21 +8,53 @@ import (
 	"strings"
 )
 
-type rgbColor struct {
+type RGBColor struct {
 	red   int
 	green int
 	blue  int
 }
 
-type pdfSettings struct {
-	color  rgbColor
-	accent rgbColor
+type PDFSettings struct {
+	color  RGBColor
+	accent RGBColor
 	short  bool
 }
 
-func parseHex(r *http.Request, name string) (color rgbColor) {
+type TechSkill struct {
+	Name      string
+	Values    string
+	Icon      string
+	BrandIcon bool
+}
+
+type MajorProject struct {
+	Company      string
+	Title        string
+	Start        string
+	End          string
+	Description  string
+	Achievements []string
+}
+
+type Person struct {
+	FirstName       string
+	LastName        string
+	Location        string
+	Phone           string
+	Email           string
+	LinkedIn        string
+	GitHub          string
+	Url             string
+	QrUrl           string
+	Summary         string
+	TechnicalSkills []TechSkill
+	JobHistory      []MajorProject
+	MajorProjects   []MajorProject
+}
+
+func parseHex(r *http.Request, name string) (color RGBColor) {
 	s := strings.TrimPrefix(r.URL.Query().Get(name), "#")
-	color = rgbColor{0, 0, 0}
+	color = RGBColor{0, 0, 0}
 
 	if len(s) != 6 {
 		return color
@@ -52,17 +84,17 @@ func resume(w http.ResponseWriter, r *http.Request) {
 		GitHub:    "axfrancis",
 		Url:       "axf.id.au/case-studies",
 		QrUrl:     "github.com/axfrancis/GO-GETAJOB",
-		Summary:   "Senior Software Developer with 8+ years of experience building high-availability critical retail infrastructure, software, and apps with a holistic philosophy, a keen eye for design, and a passion for customer experience. Proven track record modernising legacy systems, migrating software architectures, and delivering software to handle thousands of daily transactions, providing faster and more reliable results, leading to higher customer satisfaction.",
+		Summary:   "Senior Software Developer with 8+ years of experience building high-availability critical retail infrastructure, software, and apps with a holistic philosophy, a keen eye for design, and a passion for customer experience. Proven track record modernising legacy systems, migrating software architectures, and delivering software that handles thousands of daily transactions, provides faster and more reliable results, and leads to higher customer satisfaction.",
 		TechnicalSkills: []TechSkill{
 			{
 				Icon:   "\uf188",
 				Name:   "Languages",
-				Values: "TypeScript/JavaScript, Go, C#, C, Swift, Kotlin, SQL, HTML, CSS, Docker, Bash, PowerShell",
+				Values: "TypeScript/JavaScript, Go, C#, C, Swift, Kotlin, SQL",
 			},
 			{
 				Icon:   "\uf1c9",
 				Name:   "Frameworks",
-				Values: "Webpack, Node, Express, React, React Native, .NET Core",
+				Values: "Webpack, Node, Express, React, React Native, .NET Core, Docker, Bash, PowerShell",
 			},
 			{
 				Icon:      "\uf375",
@@ -76,7 +108,7 @@ func resume(w http.ResponseWriter, r *http.Request) {
 				Values: "SQL Server, MySQL/MariaDB, Redis, AWS DynamoDB",
 			},
 		},
-		JobHistory: []Job{
+		JobHistory: []MajorProject{
 			{
 				Company: "Officeworks",
 				Title:   "Senior Developer",
@@ -98,7 +130,7 @@ func resume(w http.ResponseWriter, r *http.Request) {
 				End:     "Aug 2021",
 				Achievements: []string{
 					"Migrated 5+ internal-facing HTML/CSS/JS Cordova iOS apps to React Native while maintaining 100% uptime and functionality",
-					"Designed and implemented supporting AWS architecture for those apps, automating several manual processes",
+					"Designed and implemented supporting AWS architecture for the internal apps using a code-as-architecture CloudFormation approach",
 					"Developed several internal-facing dashboards to automate previously manual processes & save stakeholders many work hours",
 					"Spearheaded the design and implementation of several Click & Collect features to reduce customer waiting time and increase NPS",
 					"Collaborated with external vendors to implement custom wrappers for hardware integrations",
@@ -112,15 +144,52 @@ func resume(w http.ResponseWriter, r *http.Request) {
 				Achievements: []string{
 					"Supported & developed internal HTML/CSS/JavaScript Cordova iOS apps",
 					"Developed & maintained Nightwatch automated tests",
-					"Investigated integrating electronic shelf-edge label technology into those apps",
+					"Investigated integrating electronic shelf-edge label technology into internal apps",
+				},
+			},
+		},
+		MajorProjects: []MajorProject{
+			{
+				Company:     "Officeworks",
+				Title:       `"Pollie" POS`,
+				Start:       "2023",
+				End:         "Present",
+				Description: `Developed in-house, Pollie is a point of sale system with a .NET Core/C# back-end, a JQuery/React JavaScript/TypeScript based front-end, and several supporting applications for interfacing with register hardware i.e. scanners, printers, and customer displays. Pollie handles thousands of daily transactions across hundreds of stores nationwide.`,
+				Achievements: []string{
+					"Refactored large parts of the JS front-end into TypeScript, which enabled the easy identification and removal of dead code and hidden bugs",
+					"Implemented custom tax logic and reporting for GST edge cases when dealing with Government entities",
+					"Performed Pentest/Audit remediation on large parts of the front and back ends to secure each endpoint, prevent prompt injection, remove hardcoded secrets from the codebase, enable the secure storage of secrets in AWS, etc.",
+				},
+			},
+			{
+				Company:     "Officeworks",
+				Title:       "Click & Collect App",
+				Start:       "2018",
+				End:         "2023",
+				Description: "Click & Collect is React Native/TypeScript based internal-facing app that enables team members in-store to pick orders, manage customer collections, and interface with 3rd party APIs e.g. DoorDash. Efficient and timely handling of Click & Collect orders is crucial to overall NPS scores, with a focus on customer time spent waiting in-store.",
+				Achievements: []string{
+					"Implemented a smart caching service to sit between the app and SAP, in order to reduce load on SAP endpoints and enable faster load times for team members",
+					"Integrated with DoorDash APIs to facilitate communication between store team members and dashers from within the app",
+					`Spearheaded customer experience initiatives such as "Valet" which enables team members at POS to notify those at back of house of when a customer has arrived and prompt them to bring the order to POS, and "I'm on my way!" which enables customers to let TMs at stores know when to bring larger orders to the front of store to await collection. Both of these initiatives resulted in a massive NPS spike for Click & Collect orders`,
+				},
+			},
+			{
+				Company:     "Self Published",
+				Title:       "Free Cell Ad-Free",
+				Start:       "2020",
+				End:         "Present",
+				Description: `Started during the COVID pandemic as a way to pass the time, Free Cell Ad-Free is a React Native/TypeScript based game, inspired by Microsoft's FreeCell: free to play card games with no interruptions.`,
+				Achievements: []string{
+					"Launched on iOS and Android, with hundreds of reviews averaging over four stars",
+					`"No ads, no frills" philosophy treats users as people, not wallets`,
 				},
 			},
 		},
 	}
 
-	settings := pdfSettings{
-		color:  rgbColor{0, 0, 0},
-		accent: rgbColor{100, 100, 100},
+	settings := PDFSettings{
+		color:  RGBColor{0, 0, 0},
+		accent: RGBColor{100, 100, 100},
 		short:  false,
 	}
 
--- pdf.go
+++ pdf.go
@@ -13,67 +13,56 @@ import (
 //go:embed fonts/*
 var assets embed.FS
 
-type TechSkill struct {
-	Name      string
-	Values    string
-	Icon      string
-	BrandIcon bool
+type PageStyle struct {
+	width, margin float64
 }
 
-type Job struct {
-	Company      string
-	Title        string
-	Start        string
-	End          string
-	Achievements []string
+type Font struct {
+	name, style, location string
 }
 
-type Person struct {
-	FirstName       string
-	LastName        string
-	Location        string
-	Phone           string
-	Email           string
-	LinkedIn        string
-	GitHub          string
-	Url             string
-	QrUrl           string
-	Summary         string
-	TechnicalSkills []TechSkill
-	JobHistory      []Job
+type SectionStyle struct {
+	iconWidth, cellHeight float64
 }
 
-func generatePDF(person Person, settings pdfSettings) ([]byte, error) {
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	margin := 12.0
-	x := margin
-	y := margin
-	pageWidth := 210.0
-	iconWidth := 5.0
+func loadFonts(pdf *gofpdf.Fpdf) {
+	fonts := []Font{
+		{"Inter", "", "fonts/Inter/static/Inter_18pt-Regular.ttf"},
+		{"Inter", "B", "fonts/Inter/static/Inter_18pt-Bold.ttf"},
+		{"Caveat", "B", "fonts/Caveat/static/Caveat-Bold.ttf"},
+		{"FASolid", "", "fonts/FontAwesome/fa-solid-900.ttf"},
+		{"FABrands", "", "fonts/FontAwesome/fa-brands-400.ttf"},
+	}
+
+	for _, f := range fonts {
+		fontBytes, fontErr := assets.ReadFile(f.location)
+
+		if fontErr != nil {
+			continue
+		}
+
+		pdf.AddUTF8FontFromBytes(f.name, f.style, fontBytes)
+	}
+}
+
+func drawLine(pdf *gofpdf.Fpdf, padding float64, pageStyle PageStyle) {
+	y := pdf.GetY() + padding
+	pdf.Line(pageStyle.margin, y, pageStyle.width-pageStyle.margin, y)
+	y += padding
+	pdf.SetY(y)
+}
+
+func generateHeader(pdf *gofpdf.Fpdf, person Person, settings PDFSettings, pageStyle PageStyle) {
+	var storedY float64
 	nameWidth := 60.0
 	contactWidth := 33.0
 	linkWidth := 48.0
-	cellHeight := 5.75
 	qrSize := 20.0
 	paddingWidth := 3.0
-	paddingHeight := 2.5
-	var storedY float64
-	interRegular, _ := assets.ReadFile("fonts/Inter/static/Inter_18pt-Regular.ttf")
-	interBold, _ := assets.ReadFile("fonts/Inter/static/Inter_18pt-Bold.ttf")
-	caveatBold, _ := assets.ReadFile("fonts/Caveat/static/Caveat-Bold.ttf")
-	faSolid, _ := assets.ReadFile("fonts/FontAwesome/fa-solid-900.ttf")
-	faBrands, _ := assets.ReadFile("fonts/FontAwesome/fa-brands-400.ttf")
+	sectionStyle := SectionStyle{5, 5.75}
+	x := pageStyle.margin
+	y := pageStyle.margin
 
-	pdf.SetMargins(margin, margin, margin)
-	pdf.SetAutoPageBreak(true, margin)
-
-	pdf.AddUTF8FontFromBytes("Inter", "", interRegular)
-	pdf.AddUTF8FontFromBytes("Inter", "B", interBold)
-	pdf.AddUTF8FontFromBytes("Caveat", "B", caveatBold)
-	pdf.AddUTF8FontFromBytes("FASolid", "", faSolid)
-	pdf.AddUTF8FontFromBytes("FABrands", "", faBrands)
-
-	// header
 	qrPng, qrErr := qrcode.EncodeWithColor("https://"+person.QrUrl, qrcode.Medium, int(qrSize*4), color.RGBA{uint8(settings.accent.red), uint8(settings.accent.green), uint8(settings.accent.blue), 255}, color.White)
 
 	if qrErr != nil {
@@ -82,109 +71,104 @@ func generatePDF(person Person, settings pdfSettings) ([]byte, error) {
 
 	pdf.RegisterImageOptionsReader("qr", gofpdf.ImageOptions{ImageType: "PNG"}, bytes.NewReader(qrPng))
 
-	pdf.AddPage()
-
 	pdf.SetXY(x, y)
 	pdf.SetFont("Inter", "B", 28)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.MultiCell(nameWidth, 9, fmt.Sprintf("%s\n%s", person.FirstName, person.LastName), "", "L", false)
 
-	x = pageWidth - margin - (linkWidth + iconWidth) - (contactWidth + iconWidth) - qrSize - (paddingWidth * 4)
+	x = pageStyle.width - pageStyle.margin - (linkWidth + sectionStyle.iconWidth) - (contactWidth + sectionStyle.iconWidth) - qrSize - (paddingWidth * 4)
 	y += 0.5
 	pdf.SetXY(x, y)
 	pdf.SetFont("FASolid", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf57e")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf57e")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(contactWidth, cellHeight, person.Location, "", 0, "R", false, 0, "")
+	pdf.CellFormat(contactWidth, sectionStyle.cellHeight, person.Location, "", 0, "R", false, 0, "")
 
 	storedY = y
-	y += cellHeight
+	y += sectionStyle.cellHeight
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("FASolid", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf879")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf879")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(contactWidth, cellHeight, person.Phone, "", 0, "R", false, 0, "")
+	pdf.CellFormat(contactWidth, sectionStyle.cellHeight, person.Phone, "", 0, "R", false, 0, "")
 
-	y += cellHeight
+	y += sectionStyle.cellHeight
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("FASolid", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf0e0")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf0e0")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(contactWidth, cellHeight, person.Email, "", 0, "R", false, 0, "")
+	pdf.CellFormat(contactWidth, sectionStyle.cellHeight, person.Email, "", 0, "R", false, 0, "")
 
 	y = storedY
-	x = pageWidth - margin - (linkWidth + iconWidth) - qrSize - (paddingWidth * 2)
+	x = pageStyle.width - pageStyle.margin - (linkWidth + sectionStyle.iconWidth) - qrSize - (paddingWidth * 2)
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("FABrands", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf08c")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf08c")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(linkWidth, cellHeight, "linkedin.com/in/"+person.LinkedIn, "", 0, "R", false, 0, "https://linkedin.com/"+person.LinkedIn)
+	pdf.CellFormat(linkWidth, sectionStyle.cellHeight, "linkedin.com/in/"+person.LinkedIn, "", 0, "R", false, 0, "https://linkedin.com/"+person.LinkedIn)
 
-	y += cellHeight
+	y += sectionStyle.cellHeight
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("FABrands", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf09b")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf09b")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(linkWidth, cellHeight, "github.com/"+person.GitHub, "", 0, "R", false, 0, "https://github.com/"+person.GitHub)
+	pdf.CellFormat(linkWidth, sectionStyle.cellHeight, "github.com/"+person.GitHub, "", 0, "R", false, 0, "https://github.com/"+person.GitHub)
 
-	y += cellHeight
+	y += sectionStyle.cellHeight
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("FASolid", "", 10)
-	pdf.Cell(iconWidth, cellHeight, "\uf5bc")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf5bc")
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 	pdf.SetFont("Inter", "", 11)
-	pdf.CellFormat(linkWidth, cellHeight, person.Url, "", 0, "R", false, 0, "https://"+person.Url)
+	pdf.CellFormat(linkWidth, sectionStyle.cellHeight, person.Url, "", 0, "R", false, 0, "https://"+person.Url)
 
 	y = storedY - 1.5
-	x = pageWidth - margin - qrSize - 1
+	x = pageStyle.width - pageStyle.margin - qrSize - 1
 	pdf.ImageOptions("qr", x, y, qrSize, qrSize, false, gofpdf.ImageOptions{}, 0, "https://"+person.QrUrl)
 
 	y = storedY - 5
-	x = pageWidth - margin - qrSize - 1 - 31.5
+	x = pageStyle.width - pageStyle.margin - qrSize - 1 - 31.5
 	pdf.SetXY(x, y)
 	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
 	pdf.SetFont("Caveat", "B", 10.5)
-	pdf.Cell(45, iconWidth, "See how this PDF was generated!")
+	pdf.Cell(45, sectionStyle.iconWidth, "See how this PDF was generated!")
 	pdf.SetFont("FASolid", "", 10)
-	pdf.Cell(iconWidth, iconWidth, "\uf0ab")
+	pdf.Cell(sectionStyle.iconWidth, sectionStyle.iconWidth, "\uf0ab")
 	pdf.SetFont("Inter", "", 11)
 	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 
-	y = storedY - 1.5 + (cellHeight * 3)
-	x = pageWidth - margin - qrSize - paddingWidth
+	y = storedY - 1.5 + (sectionStyle.cellHeight * 3)
+	x = pageStyle.width - pageStyle.margin - qrSize - paddingWidth
 	pdf.SetDrawColor(225, 225, 225)
 	pdf.Line(x, storedY+2.5, x, y-2.5)
-	x -= linkWidth + iconWidth + (paddingWidth * 2)
+	x -= linkWidth + sectionStyle.iconWidth + (paddingWidth * 2)
 	pdf.Line(x, storedY+2.5, x, y-2.5)
+	pdf.SetY(y)
+}
 
-	y += paddingHeight + 1.5
-	pdf.Line(margin, y, pageWidth-margin, y)
-
-	// body
-	x = margin
-	y += paddingHeight
-	cellHeight = 5
-	pdf.SetXY(x, y)
+func generateSummary(pdf *gofpdf.Fpdf, person Person) {
+	sectionStyle := SectionStyle{
+		cellHeight: 5,
+	}
 	pdf.SetFontSize(12)
-	pdf.MultiCell(0, cellHeight, person.Summary, "", "J", false)
+	pdf.MultiCell(0, sectionStyle.cellHeight, person.Summary, "", "J", false)
+}
 
-	y = pdf.GetY() + paddingHeight
-	pdf.Line(margin, y, pageWidth-margin, y)
-
-	y += paddingHeight
-	pdf.SetXY(x, y)
-	cellHeight += 0.5
-
+func generateTechSkills(pdf *gofpdf.Fpdf, person Person, settings PDFSettings, pageStyle PageStyle) {
+	sectionStyle := SectionStyle{
+		iconWidth:  5,
+		cellHeight: 5.5,
+	}
 	for _, c := range person.TechnicalSkills {
 		var icon string
 		if c.Icon == "" {
@@ -192,9 +176,9 @@ func generatePDF(person Person, settings pdfSettings) ([]byte, error) {
 		} else {
 			icon = c.Icon
 		}
-		pdf.SetX(margin)
+		pdf.SetX(pageStyle.margin)
 		pdf.SetFont("Inter", "B", 11)
-		pdf.MultiCell(0, cellHeight, c.Name, "", "L", false)
+		pdf.MultiCell(0, sectionStyle.cellHeight, c.Name, "", "L", false)
 		if c.BrandIcon {
 			pdf.SetFont("FABrands", "", 10)
 		} else {
@@ -202,49 +186,110 @@ func generatePDF(person Person, settings pdfSettings) ([]byte, error) {
 		}
 		pdf.SetX(pdf.GetX() + 3.5)
 		pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
-		pdf.CellFormat(iconWidth, iconWidth, icon, "", 0, "C", false, 0, "")
+		pdf.CellFormat(sectionStyle.iconWidth, sectionStyle.iconWidth, icon, "", 0, "C", false, 0, "")
 		pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
 		pdf.SetFont("Inter", "", 11)
 		pdf.SetX(pdf.GetX() + 2)
-		pdf.MultiCell(0, cellHeight, c.Values, "", "L", false)
+		pdf.MultiCell(0, sectionStyle.cellHeight, c.Values, "", "L", false)
 	}
+}
 
-	y = pdf.GetY() + paddingHeight
-	pdf.Line(margin, y, pageWidth-margin, y)
-	y += paddingHeight
-	pdf.SetY(y)
-
-	cellHeight = 5
+func generateJobHistory(pdf *gofpdf.Fpdf, person Person, settings PDFSettings, pageStyle PageStyle) {
+	sectionStyle := SectionStyle{
+		iconWidth:  5,
+		cellHeight: 5,
+	}
 
 	for _, j := range person.JobHistory {
 		pdf.SetFontSize(12)
 		pdf.SetFontStyle("B")
-		pdf.Cell(30, cellHeight, j.Company)
+		pdf.Cell(30, sectionStyle.cellHeight, j.Company)
 		pdf.SetFontSize(11)
 		pdf.SetFontStyle("")
 		pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
-		pdf.Cell(30, cellHeight, j.Start+" - "+j.End)
+		pdf.Cell(30, sectionStyle.cellHeight, j.Start+" - "+j.End)
 		pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
-		pdf.SetXY(margin, pdf.GetY()+cellHeight)
+		pdf.SetXY(pageStyle.margin, pdf.GetY()+sectionStyle.cellHeight)
 		pdf.SetFontStyle("B")
-		pdf.MultiCell(0, cellHeight+1, j.Title, "", "L", false)
+		pdf.MultiCell(0, sectionStyle.cellHeight+1, j.Title, "", "L", false)
 		pdf.SetFontStyle("")
 
 		for _, a := range j.Achievements {
-			pdf.SetXY(margin+2, pdf.GetY()+0.75)
+			pdf.SetXY(pageStyle.margin+2, pdf.GetY()+0.75)
 			pdf.SetFont("FASolid", "", 4)
-			pdf.CellFormat(iconWidth, cellHeight, "\uf111", "", 0, "C", false, 0, "")
+			pdf.CellFormat(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf111", "", 0, "C", false, 0, "")
 			pdf.SetFont("Inter", "", 11)
-			pdf.MultiCell(0, cellHeight, a, "", "L", false)
+			pdf.MultiCell(0, sectionStyle.cellHeight, a, "", "L", false)
 		}
 
-		pdf.SetXY(margin, pdf.GetY()+3.0)
+		pdf.SetXY(pageStyle.margin, pdf.GetY()+3.0)
 	}
+}
 
-	y = pdf.GetY() + paddingHeight
-	pdf.Line(margin, y, pageWidth-margin, y)
-	y += paddingHeight
-	pdf.SetY(y)
+func generateMajorProjects(pdf *gofpdf.Fpdf, person Person, settings PDFSettings, pageStyle PageStyle) {
+	sectionStyle := SectionStyle{
+		cellHeight: 5,
+		iconWidth:  5,
+	}
+	pdf.SetFontSize(14)
+	pdf.SetFontStyle("B")
+	pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
+	pdf.MultiCell(0, sectionStyle.cellHeight+5, "Major Projects", "", "L", false)
+	pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
+
+	for _, j := range person.MajorProjects {
+		pdf.SetFontSize(12)
+		pdf.SetFontStyle("B")
+		pdf.Cell(50, sectionStyle.cellHeight, j.Title)
+		pdf.SetFontStyle("")
+		pdf.SetFontSize(11)
+		pdf.SetTextColor(settings.accent.red, settings.accent.green, settings.accent.blue)
+		pdf.Cell(35, sectionStyle.cellHeight, j.Company)
+		pdf.Cell(30, sectionStyle.cellHeight, j.Start+" - "+j.End)
+		pdf.SetTextColor(settings.color.red, settings.color.green, settings.color.blue)
+		pdf.SetXY(pageStyle.margin, pdf.GetY()+sectionStyle.cellHeight+1)
+		pdf.MultiCell(0, sectionStyle.cellHeight-0.5, j.Description, "", "J", false)
+
+		for _, a := range j.Achievements {
+			pdf.SetXY(pageStyle.margin+2, pdf.GetY()+0.75)
+			pdf.SetFont("FASolid", "", 4)
+			pdf.CellFormat(sectionStyle.iconWidth, sectionStyle.cellHeight, "\uf111", "", 0, "C", false, 0, "")
+			pdf.SetFont("Inter", "", 11)
+			pdf.MultiCell(0, sectionStyle.cellHeight-0.5, a, "", "L", false)
+		}
+
+		pdf.SetXY(pageStyle.margin, pdf.GetY()+3.0)
+	}
+}
+
+func generatePDF(person Person, settings PDFSettings) ([]byte, error) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pageStyle := PageStyle{210, 12}
+	paddingHeight := 2.5
+
+	loadFonts(pdf)
+
+	pdf.SetMargins(pageStyle.margin, pageStyle.margin, pageStyle.margin)
+	pdf.SetAutoPageBreak(true, pageStyle.margin)
+
+	pdf.AddPage()
+
+	generateHeader(pdf, person, settings, pageStyle)
+	drawLine(pdf, paddingHeight, pageStyle)
+
+	generateSummary(pdf, person)
+	drawLine(pdf, paddingHeight, pageStyle)
+
+	generateTechSkills(pdf, person, settings, pageStyle)
+	drawLine(pdf, paddingHeight, pageStyle)
+
+	generateJobHistory(pdf, person, settings, pageStyle)
+	drawLine(pdf, paddingHeight, pageStyle)
+
+	if settings.short != true {
+		generateMajorProjects(pdf, person, settings, pageStyle)
+		drawLine(pdf, paddingHeight, pageStyle)
+	}
 
 	var buffer bytes.Buffer
 
